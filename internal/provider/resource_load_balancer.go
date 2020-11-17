40b0b29fdd6a6354c1cbc51072cafb275a35c20f
Jim Myhrberg <contact@jimeh.me>
2020-11-17 15:29:17 +0000

    chore(load_balancer): improve helper method names and signature

--- internal/provider/resource_load_balancer.go
+++ internal/provider/resource_load_balancer.go
@@ -111,7 +111,7 @@ func resourceLoadBalancerCreate(
 		name = namegenerator.RandomName("tf")
 	}
 
-	t, ids := extractBalancerResourceTypeAndIDs(d, m)
+	t, ids := extractLoadBalancerResourceTypeAndIDs(d, m)
 	if t == "" {
 		t = katapult.VirtualMachinesResourceType
 	}
@@ -156,7 +156,7 @@ func resourceLoadBalancerRead(
 
 	_ = d.Set("name", lb.Name)
 	_ = d.Set("resource_type", string(lb.ResourceType))
-	parseLoadBalancerResourceTypeAndIDs(lb.ResourceType, lb.ResourceIDs, d)
+	populateLoadBalancerTargets(d, lb.ResourceType, lb.ResourceIDs)
 	_ = d.Set("https_redirect", lb.HTTPSRedirect)
 	if lb.IPAddress != nil {
 		_ = d.Set("ip_address", lb.IPAddress.Address)
@@ -181,7 +181,7 @@ func resourceLoadBalancerUpdate(
 	}
 
 	if d.HasChanges("virtual_machine", "virtual_machine_group", "tag") {
-		t, ids := extractBalancerResourceTypeAndIDs(d, m)
+		t, ids := extractLoadBalancerResourceTypeAndIDs(d, m)
 		lb.ResourceType = t
 		lb.ResourceIDs = ids
 	}
@@ -211,10 +211,10 @@ func resourceLoadBalancerDelete(
 	return diag.Diagnostics{}
 }
 
-func parseLoadBalancerResourceTypeAndIDs(
+func populateLoadBalancerTargets(
+	d *schema.ResourceData,
 	t katapult.ResourceType,
 	ids []string,
-	d *schema.ResourceData,
 ) {
 	list := []map[string]string{}
 	for _, id := range ids {
@@ -231,7 +231,7 @@ func parseLoadBalancerResourceTypeAndIDs(
 	}
 }
 
-func extractBalancerResourceTypeAndIDs(
+func extractLoadBalancerResourceTypeAndIDs(
 	d *schema.ResourceData,
 	m interface{},
 ) (katapult.ResourceType, []string) {
