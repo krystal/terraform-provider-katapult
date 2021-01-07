2ca053a3a10cb1670ab47aed7c3164fb35d216f5
Jim Myhrberg <contact@jimeh.me>
2021-01-07 17:31:21 +0000

    fix(errors): handle nil *katapult.Response.Response values
    This is a dirty workaround until errors are refactored in the katapult
    package to be compatible with errors.Is().

--- internal/provider/resource_load_balancer.go
+++ internal/provider/resource_load_balancer.go
@@ -135,7 +135,7 @@ func resourceLoadBalancerRead(
 
 	lb, resp, err := c.LoadBalancers.GetByID(ctx, id)
 	if err != nil {
-		if resp != nil && resp.StatusCode == 404 {
+		if resp != nil && resp.Response != nil && resp.StatusCode == 404 {
 			d.SetId("")
 
 			return diags
