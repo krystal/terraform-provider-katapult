4a75794d3a0c7c46902b8d27b6ff60dead9bb419
Jim Myhrberg <contact@jimeh.me>
2021-02-18 18:22:48 +0000

    chore(test): add unit test for (*Meta) UseOrGenerateHostname() function

--- internal/provider/meta_test.go
+++ internal/provider/meta_test.go
@@ -53,9 +53,58 @@ func TestMeta_UseOrGenerateName(t *testing.T) {
 
 			got := m.UseOrGenerateName(tt.args.name)
 
-			assert.Truef(t, tt.want.MatchString(got),
-				"expected \"%s\" to match \"%s\"", got, tt.want,
-			)
+			assert.Regexp(t, tt.want, got)
+		})
+	}
+}
+
+func TestMeta_UseOrGenerateHostname(t *testing.T) {
+	type fields struct {
+		GeneratedNamePrefix string
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *regexp.Regexp
+	}{
+		{
+			name:   "no prefix and empty string",
+			fields: fields{},
+			args:   args{name: ""},
+			want:   regexp.MustCompile("^[^-]+-[^-]+-[^-]+$"),
+		},
+		{
+			name:   "no prefix and non-empty string",
+			fields: fields{},
+			args:   args{name: "dope-groovy-narwhal-flower"},
+			want:   regexp.MustCompile("^dope-groovy-narwhal-flower$"),
+		},
+		{
+			name:   "prefix and empty string",
+			fields: fields{GeneratedNamePrefix: "tf-unit-test"},
+			args:   args{name: ""},
+			want:   regexp.MustCompile("^tf-unit-test-[^-]+-[^-]+-[^-]+$"),
+		},
+		{
+			name:   "prefix and non-empty string",
+			fields: fields{GeneratedNamePrefix: "tf-unit-test"},
+			args:   args{name: "dope-groovy-narwhal-flower"},
+			want:   regexp.MustCompile("^dope-groovy-narwhal-flower$"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Meta{
+				GeneratedNamePrefix: tt.fields.GeneratedNamePrefix,
+			}
+
+			got := m.UseOrGenerateHostname(tt.args.name)
+
+			assert.Regexp(t, tt.want, got)
 		})
 	}
 }
