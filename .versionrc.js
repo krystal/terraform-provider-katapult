719086535615608540aae4acb360d5b5f988c31a
Jim Myhrberg <contact@jimeh.me>
2021-02-25 20:08:19 +0000

    chore: add .versionrc.js to customize changelog to include "docs" type commits

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  packageFiles: [],
+  bumpFiles: [],
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "docs", section: "Documentation" },
+    { type: "chore", hidden: true },
+    { type: "style", hidden: true },
+    { type: "refactor", hidden: true },
+    { type: "perf", hidden: true },
+    { type: "test", hidden: true },
+  ],
+};
