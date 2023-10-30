a3ffcd6d63793273ca363fe17880a518d700e00b
Bill Gale <bill@gale.dev>
2023-10-30 10:51:51 +0000

    chore: add v6 mux

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"context"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/krystal/terraform-provider-katapult/internal/provider"
 )
 
@@ -11,10 +17,33 @@ var (
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: provider.New(&provider.Config{
+	ctx := context.Background()
+
+	upgradedSDKServer, err := tf5to6server.UpgradeServer(
+		ctx, provider.New(&provider.Config{
 			Version: version,
 			Commit:  commit,
-		}),
-	})
+		})().GRPCProvider,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer { return upgradedSDKServer },
+	}
+
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf6server.Serve(
+		"registry.terraform.io/providers/krystal/katapult",
+		muxServer.ProviderServer,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
