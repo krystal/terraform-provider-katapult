374f0c923cd5eec5bbc2bee14d2b3345426ebf9f
Bill Gale <bill@gale.dev>
2024-03-25 12:12:26 +0000

    chore(doc): update examples

--- examples/resources/katapult_load_balancer_rule/resource.tf
+++ examples/resources/katapult_load_balancer_rule/resource.tf
@@ -1,3 +1,5 @@
+# Use a seperate `katapult_load_balancer_rule` resource to add rules to an
+# existing `katapult_load_balancer`.
 resource "katapult_load_balancer" "my_lb" {
   name = "vm"
   external_rules = true
@@ -9,4 +11,25 @@ resource "katapult_load_balancer_rule" "my_rule" {
 	listen_port = 80
 	protocol = "HTTP"
 	passthrough_ssl = false
+}
+
+# Complete example of a load balancer rule 
+
+resource "katapult_load_balancer_rule" "complete_rule" {
+	load_balancer_id = katapult_load_balancer.my_lb.id
+	destination_port = 8443
+	listen_port = 443
+	protocol = "HTTPS"
+	algorithm = "sticky"
+	passthrough_ssl = true
+	backend_ssl = true
+	proxy_protocol = true
+	check_enabled = true
+	check_fall = 2
+	check_interval = 20
+	check_http_statuses = "2"
+	check_path = "/healthz"
+	check_rise = 2
+	check_timeout = 5
+	check_protocol = "HTTP"
 }
