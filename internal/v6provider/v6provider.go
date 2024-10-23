2008c76e0904fe785738179f01f8b9494ec4a4de
Bill Gale <bill@gale.dev>
2024-10-23 15:02:17 +0100

    feat: initial pass at object storage bucket resource

--- internal/v6provider/v6provider.go
+++ internal/v6provider/v6provider.go
@@ -248,6 +248,7 @@ func (k *KatapultProvider) Resources(
 		func() resource.Resource { return &LoadBalancerRuleResource{} },
 		func() resource.Resource { return &VirtualNetworkResource{} },
 		func() resource.Resource { return &TagResource{} },
+		func() resource.Resource { return &ObjectStorageBucketResource{} },
 	}
 }
 
